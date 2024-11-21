10961bea11c5c8bb9a3239ab63de1541fd0109fb
m4rkbello <markbello10041999@gmail.com>
2024-11-21 23:08:51 +0800

    ADDED

--- frontend_reactJS/src/components/Auth/employee/EmployeeRegister.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeRegister.jsx
@@ -476,27 +476,17 @@ const EmployeeRegister = ({ registerEmployee }) => {
         </form>
 
         <div className="carousel carousel-vertical rounded-box h-96">
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" />
-          </div>
+        <div className="bg-base-200 collapse">
+        <input type="checkbox" className="peer" />
+        <div
+          className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Click me to show/hide content
+        </div>
+        <div
+          className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>hello</p>
+        </div>
+      </div>
         </div>
       </div>
     </div>
