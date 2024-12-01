4ffb59989068033a71b2ea00d1e6149f05e920d0
m4rkbello <markbello10041999@gmail.com>
2024-12-01 17:28:14 +0800

    ADDED

--- backend_laravelPHP/database/migrations/2024_04_06_202431_create_attendances_table.php
+++ backend_laravelPHP/database/migrations/2024_04_06_202431_create_attendances_table.php
@@ -20,15 +20,7 @@ return new class extends Migration
             $table->rememberToken()->nullable();
             $table->foreignId('attendance_employee_id')->constrained('employees');
             $table->unsignedBigInteger('created_by')->nullable();
-            $table->unsignedBigInteger('updated_by')->nullable();
-            $table->foreign('created_by')
-                ->references('id')
-                ->on('users')
-                ->onDelete('set null');
-            $table->foreign('updated_by')
-                ->references('id')
-                ->on('users')
-                ->onDelete('set null');
+            $table->unsignedBigInteger('updated_by')->nullable();      
             $table->timestamps();
         });
     }
