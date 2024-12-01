389f41c412a05b35f728f8942571ad3e36c88f27
m4rkbello <markbello10041999@gmail.com>
2024-12-01 17:28:42 +0800

    ADDED

--- backend_laravelPHP/database/migrations/2024_10_14_123724_create_civilstatuses_table.php
+++ backend_laravelPHP/database/migrations/2024_10_14_123724_create_civilstatuses_table.php
@@ -17,9 +17,7 @@ return new class extends Migration
             $table->string('civil_status_description', 255)->nullable();
             $table->integer('civil_status_status_id')->nullable();;
             $table->unsignedBigInteger('created_by')->nullable();
-            $table->unsignedBigInteger('updated_by')->nullable();
-            $table->foreign('created_by')->references('id')->on('users'); 
-            $table->foreign('updated_by')->references('id')->on('users'); 
+            $table->unsignedBigInteger('updated_by')->nullable();      
             $table->timestamps();
         });
     }
