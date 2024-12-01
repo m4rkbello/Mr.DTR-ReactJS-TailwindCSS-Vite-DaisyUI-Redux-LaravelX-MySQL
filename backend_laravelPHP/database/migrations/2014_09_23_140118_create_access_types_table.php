bb710c9b72d9092f9542e16ba78301e9a3b998cd
m4rkbello <markbello10041999@gmail.com>
2024-12-01 17:25:29 +0800

    ADDED

--- backend_laravelPHP/database/migrations/2014_09_23_140118_create_access_types_table.php
+++ backend_laravelPHP/database/migrations/2014_09_23_140118_create_access_types_table.php
@@ -14,18 +14,7 @@ return new class extends Migration
             $table->string('access_type_description')->nullable();
             $table->integer('access_type_status_id')->nullable();
             $table->unsignedBigInteger('created_by')->nullable();
-            $table->unsignedBigInteger('updated_by')->nullable();
-     
-            $table->foreign('created_by')
-                ->references('id')
-                ->on('users')
-                ->onDelete('set null');
-        
-            $table->foreign('updated_by')
-                ->references('id')
-                ->on('users')
-                ->onDelete('set null');
-            
+            $table->unsignedBigInteger('updated_by')->nullable();      
             $table->timestamps();
         });
         
