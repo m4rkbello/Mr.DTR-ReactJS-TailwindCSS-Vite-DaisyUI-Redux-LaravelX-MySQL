88b1058fde03ed92551cc54720218d42483680c7
m4rkbello <markbello10041999@gmail.com>
2024-12-08 20:53:08 +0800

    ADD PHP ARTISAN MAKE:OBSERVER FOR ACTIVITY LOGGINGS

--- backend_laravelPHP/app/Models/ActivityLogs.php
+++ backend_laravelPHP/app/Models/ActivityLogs.php
@@ -8,4 +8,55 @@ use Illuminate\Database\Eloquent\Model;
 class ActivityLogs extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'activity_logs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'activity',
+        'table_name',
+        'record_id',
+        'created_by_user_id',
+        'created_by_employee_id',
+        'updated_by_user_id',
+        'updated_by_employee_id',
+    ];
+
+    /**
+     * Relationships.
+     */
+
+    // Created by User
+    public function createdByUser()
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
+
+    // Created by Employee
+    public function createdByEmployee()
+    {
+        return $this->belongsTo(Employee::class, 'created_by_employee_id');
+    }
+
+    // Updated by User
+    public function updatedByUser()
+    {
+        return $this->belongsTo(User::class, 'updated_by_user_id');
+    }
+
+    // Updated by Employee
+    public function updatedByEmployee()
+    {
+        return $this->belongsTo(Employee::class, 'updated_by_employee_id');
+    }
 }
