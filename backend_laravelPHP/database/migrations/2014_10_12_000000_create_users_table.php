2edbdc37b1074564f8cc9d4761974fbadb32d7f9
m4rkbello <markbello10041999@gmail.com>
2025-01-11 09:25:38 +0800

    ADDED

--- backend_laravelPHP/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend_laravelPHP/database/migrations/2014_10_12_000000_create_users_table.php
@@ -39,6 +39,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeignIfExists('access_type_id');
         });
+        
         Schema::dropIfExists('users');
     }
 };
